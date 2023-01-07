99e600fb5672697ec391bec1665191c1a1cd3499
Cubik <cubik65536@cubik65536.top>
2023-01-07 16:56:00 -0500

    feat: Write Minecraft Item IDs to JSON file

--- src/main/java/tech/ixor/minecraft_item_data_getter/MinecraftItemDataGetter.java
+++ src/main/java/tech/ixor/minecraft_item_data_getter/MinecraftItemDataGetter.java
@@ -1,7 +1,14 @@
 package tech.ixor.minecraft_item_data_getter;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import net.minecraft.util.registry.Registry;
 import net.fabricmc.api.ModInitializer;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class MinecraftItemDataGetter implements ModInitializer {
 	@Override
@@ -12,10 +19,26 @@ public class MinecraftItemDataGetter implements ModInitializer {
 
 		System.out.println("Hello Fabric world!");
 
+		JsonArray items = new JsonArray();
+
 		System.out.println("Printing Minecraft Item Registry Data to console...");
 		Registry.ITEM.forEach(item -> {
-			System.out.println("Minecraft Item: " + item.toString());
+			String itemName = item.toString();
+			System.out.println("Minecraft Item: " + itemName);
+			items.add(itemName);
 		});
 		System.out.println("Printed Minecraft Item Registry Data to console.");
+
+		System.out.println("Writing Minecraft Item Registry Data to file...");
+		JsonObject minecraftItemData = new JsonObject();
+		minecraftItemData.add("minecraft_item", items);
+
+		try (FileWriter file = new FileWriter("MinecraftItemData.json")) {
+			System.out.println("File Path: " + Paths.get("MinecraftItemData.json").toAbsolutePath());
+			file.write(minecraftItemData.toString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
