23e41bd6dba8ce6ac7b9661779f5b533b08c3c25
Cubik <cubik65536@cubik65536.top>
2023-01-07 17:13:34 -0500

    feat: Print legacy item IDs & Write to JSON

--- src/main/java/tech/ixor/minecraft_item_data_getter/MinecraftItemDataGetter.java
+++ src/main/java/tech/ixor/minecraft_item_data_getter/MinecraftItemDataGetter.java
@@ -2,15 +2,21 @@ package tech.ixor.minecraft_item_data_getter;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.io.Reader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 
 import net.minecraft.util.registry.Registry;
 import net.fabricmc.api.ModInitializer;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MinecraftItemDataGetter implements ModInitializer {
 	@Override
@@ -41,6 +47,22 @@ public class MinecraftItemDataGetter implements ModInitializer {
 			itemNames.add(blockName);
 		});
 		System.out.println("Printed Minecraft Block Registry Data to console.");
+
+
+		Path legacyItemDataPath = Paths.get("legacy_items.json");
+		System.out.println("Reading legacy item data from " + legacyItemDataPath.toAbsolutePath() + "...");
+		try (Reader reader = Files.newBufferedReader(legacyItemDataPath, StandardCharsets.UTF_8)) {
+            JsonParser parser = new JsonParser();
+            JsonElement legacyItems = parser.parse(reader);
+			JsonArray legacyItemsArray = legacyItems.getAsJsonArray();
+            for (JsonElement legacyItem: legacyItemsArray) {
+                if (legacyItem.isJsonObject()) {
+                    itemNames.add(legacyItem.getAsJsonObject().get("id").getAsString());
+                }
+            }
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		itemNames.sort(String::compareToIgnoreCase);
 
